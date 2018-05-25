478c9f8c448447706fe0aeb2e2afcdfd36fafbda
Elia Maino <eliamaino@outlook.com>
2018-05-25 20:54:49 +0200

    express routes controller structure

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-
-app.get('/', (req, res) => res.send('Hello World!'))
+require('./src/routes/routes.js')
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
---
+++ src/routes/routes.js
@@ -0,0 +1,3 @@
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/get-parking-areas', (req, res) => res.send('we are getting the areasssss'))
