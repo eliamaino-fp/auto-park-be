ba15e61f088dc79d41311779ded6d8a53ffa86e2
Elia Maino <eliamaino@outlook.com>
2018-05-26 02:54:58 +0200

    a

--- src/controllers/areas.js
+++ src/controllers/areas.js
@@ -2,32 +2,76 @@ function getAllAreas() {
   const areas = [
     {
       areaId : '1',
-      freeParkingPercentage : 17,
+      freeParkingPercentage : 42,
+      coordinates: [
+          [48.85, 2.4022],
+          [48.85, 2.3722],
+          [48.81, 2.3722],
+          [48.81, 2.43]
+      ],
       dropOuts : [
         {
-          xCoor : 250,
-          yCoor : 198
-        },
-        {
-          xCoor : 150,
-          yCoor : 33
+          xCoor : 48.84,
+          yCoor : 2.3922
         }
       ]
     },
     {
       areaId : '2',
-      freeParkingPercentage : 41,
+      freeParkingPercentage : 12,
+      coordinates: [
+          [48.85, 2.3722],
+          [48.85, 2.34228],
+          [48.81, 2.3222],
+          [48.81, 2.3722]
+      ],
       dropOuts : [
         {
-          xCoor : 12,
-          yCoor : 77
-        },
-        {
-          xCoor : 75,
-          yCoor : 52
+          xCoor : 48.835,
+          yCoor : 2.3622
         }
       ]
-    }
+    },
+    {
+      areaId : '3',
+      freeParkingPercentage : 25,
+      coordinates: [
+          [48.865, 2.43],
+          [48.86, 2.4022],
+          [48.85, 2.4022],
+          [48.81, 2.43]
+      ],
+      dropOuts : []
+    },
+    {
+      areaId : '4',
+      freeParkingPercentage : 77,
+      coordinates: [
+          [48.865, 2.43],
+          [48.86, 2.4022],
+          [48.85, 2.4022],
+      ],
+      dropOuts : []
+    },
+    {
+      areaId : '5',
+      freeParkingPercentage : 90,
+      coordinates: [
+          [48.845, 2.2922],
+          [48.85, 2.34228],
+          [48.81, 2.3222],
+      ],
+      dropOuts : [
+        {
+          xCoor : 48.828,
+          yCoor : 2.3122
+        },
+        {
+          xCoor : 48.83,
+          yCoor : 2.3222
+        }
+      ]
+    },
   ]
 
   return areas
